36588984f5e75eb46b837f0dd0c6f0dfa301e833
Oliver Brook <olibrook@gmail.com>
2019-12-10 15:57:22 -0800

    WIP

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{message}</p>
-        <p>Platform assessment day!</p>
+        <p>Platform assessment day again!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
